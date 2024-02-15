c2c55c6816ccb15c60e7cd09209e73a46e059363
Mohammad Asif <mohammed.asif@vbt.com.tr>
2024-02-15 23:50:17 +0300

    enter key

--- firebase-shopping/index.js
+++ firebase-shopping/index.js
@@ -1,9 +1,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
+import {
+  getDatabase,
+  ref,
+  push,
+  onValue,
+  remove,
+} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js";
 
 const appSettings = {
-	databaseURL: "https://shopping-c936f-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+  databaseURL:
+    "https://shopping-c936f-default-rtdb.europe-west1.firebasedatabase.app/",
+};
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
@@ -14,38 +21,44 @@ const btn = document.getElementById("btn");
 const input = document.getElementById("name-id");
 const shoppingList = document.getElementById("shopping-list");
 
-btn.addEventListener("click", function(){
-	let value = input.value;
-	if(value.trim() == '') return;
-	console.log("the value is: ", value);
-	push(shoppingListInDb, value);
-	input.value = "";
+btn.addEventListener("click", sendData);
+input.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    sendData();
+  }
 });
 
-onValue(shoppingListInDb, function(snapshot){
-	
-	if(!snapshot.exists()){
-		shoppingList.innerHTML = 'No Items Exists';
-		return;
-	} 
-	const valList = Object.entries(snapshot.val());
-	console.log("the snapshot is: ", valList);
-	shoppingList.innerHTML = '';
-	valList.forEach((item)=>{
-		// shoppingList.innerHTML += `<li>${val}</li>`;
-		appendItemToShoppingListEl(item);
-	});
-	window.scrollTo(0, shoppingList.scrollHeight);
+function sendData() {
+  let value = input.value;
+  if (value.trim() == "") return;
+  console.log("the value is: ", value);
+  push(shoppingListInDb, value);
+  input.value = "";
+}
+
+onValue(shoppingListInDb, function (snapshot) {
+  if (!snapshot.exists()) {
+    shoppingList.innerHTML = "No Items Exists";
+    return;
+  }
+  const valList = Object.entries(snapshot.val());
+  console.log("the snapshot is: ", valList);
+  shoppingList.innerHTML = "";
+  valList.forEach((item) => {
+    // shoppingList.innerHTML += `<li>${val}</li>`;
+    appendItemToShoppingListEl(item);
+  });
+  window.scrollTo(0, shoppingList.scrollHeight);
 });
 
-function appendItemToShoppingListEl(item){
-	const itemVal = item[1];
-	let newEl = document.createElement("li");
-	newEl.textContent = item[1];
-	newEl.addEventListener("click", function(){
-		// alert("a click happened: "+item[0]);
-		const itemRef = ref(database, `shoppingList/${item[0]}`);
-		remove(itemRef);
-	});
-	shoppingList.append(newEl);
+function appendItemToShoppingListEl(item) {
+  const itemVal = item[1];
+  let newEl = document.createElement("li");
+  newEl.textContent = item[1];
+  newEl.addEventListener("click", function () {
+    // alert("a click happened: "+item[0]);
+    const itemRef = ref(database, `shoppingList/${item[0]}`);
+    remove(itemRef);
+  });
+  shoppingList.append(newEl);
 }
